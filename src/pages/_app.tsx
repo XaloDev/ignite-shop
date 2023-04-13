047d5e124a0eaa597763c3b185d71af3a80bc77e
Ícaro Xavier <icarogabriel999@icloud.com>
2023-04-13 18:36:27 -0300

    stripe integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,20 @@
+import { globalStyles } from '@ignite-shop/styles/global'
 import type { AppProps } from 'next/app'
+import logoImg from '@ignite-shop/assets/logo.svg'
+import { Container, Header } from '@ignite-shop/styles/pages/app'
+import Image from 'next/image'
+
+import 'keen-slider/keen-slider.min.css'
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" priority quality={100} />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
