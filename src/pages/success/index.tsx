3661e75468ff3c1709d26958ddea617b2fccb24a
Ícaro Xavier <icarogabriel999@icloud.com>
2023-04-12 23:18:07 -0300

    ssr styles done

---
+++ src/pages/success/index.tsx
@@ -0,0 +1,7 @@
+export default function SuccessPage() {
+  return (
+    <main>
+      <h1>Success!</h1>
+    </main>
+  )
+}
---
+++ src/styles/index.ts
@@ -0,0 +1,16 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  config,
+  styled,
+  css,
+  getCssText,
+  globalCss,
+  keyframes,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: { rocketseat: '#8257e6' },
+  },
+})
